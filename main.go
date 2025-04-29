8fd9e0b84dd6767b8431ce42be74d87cf2cb7dd6
Cyril TAVIAN <c.tavian@outlook.fr>
2025-04-29 21:38:00 +0200

    feat(provider): Init squeleton for provider terraform

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/tacy-OPS/terraform-provider-valohai/valohai"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: valohai.Provider,
+	})
+}
